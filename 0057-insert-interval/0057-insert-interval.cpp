54126e71b483c17ff80300b6ef831a042ec5390e
Naimuddin <129611596+naimuddin23@users.noreply.github.com>
2024-03-17 14:41:12 +0530

    Time: 11 ms (65.36%), Space: 20.3 MB (93.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-         vector<vector<int>> output;
-        if(intervals.size()==0){//edge case1
-            output.push_back(newInterval);
-        return output;
-        }
-        int flag=1;
-        for (int i=0;i<intervals.size();i++){
-            if(intervals[i][0]>newInterval[0]){
-                intervals.insert(intervals.begin() + i,newInterval);
-                flag=0;
-            break;
-            }
-        }
-        if (flag==1)//edge case2
-            intervals.push_back(newInterval);
+   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size(), i = 0;
+        vector<vector<int>> res;
+
+        // Case 1: no overlapping case before the merge intervals  
+        // Compare ending point of intervals to starting point of newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            i++;
+        }    
         
-        output.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            if(output.back()[1]>=intervals[i][0])
-                output.back()[1]=max(output.back()[1],intervals[i][1]);
-            else
-                output.push_back(intervals[i]);
+        // Case 2: overlapping case and merging of intervals
+        while (i < n && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        return output;
+        res.push_back(newInterval);
         
+        // Case 3: no overlapping of intervals after newinterval being merged
+        while (i < n) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+        return res;
     }
 };
